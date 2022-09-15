f412056833ea19b04ae8442b31196e6fe1242c15
Alex Li <github@alexv525.com>
2022-09-16 01:30:47 +0800

    ⚡️ Better `runApp`

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -15,56 +15,69 @@ import 'package:flutter_ume_kit_dio/flutter_ume_kit_dio.dart';
 
 final Dio dio = Dio()..options = BaseOptions(connectTimeout: 10000);
 
-void main() {
-  if (kDebugMode) {
-    PluginManager.instance
-      ..register(WidgetInfoInspector())
-      ..register(WidgetDetailInspector())
-      ..register(ColorSucker())
-      ..register(AlignRuler())
-      ..register(ColorPicker())
-      ..register(TouchIndicator())
-      ..register(Performance())
-      ..register(ShowCode())
-      ..register(MemoryInfoPage())
-      ..register(CpuInfoPage())
-      ..register(DeviceInfoPanel())
-      ..register(Console())
-      ..register(DioInspector(dio: dio));
-    runApp(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (_) => UMESwitch()),
-        ],
-        builder: (ctx, child) => UMEWidget(
-          enable: ctx.watch<UMESwitch>().enable,
-          child: MyApp(),
-        ),
-      ),
-    );
-  } else {
-    runApp(MyApp());
-  }
+void main() => runApp(const UMEApp());
+
+class UMEApp extends StatefulWidget {
+  const UMEApp({Key? key}) : super(key: key);
+
+  @override
+  State<UMEApp> createState() => _UMEAppState();
 }
 
-class MyApp extends StatelessWidget {
+class _UMEAppState extends State<UMEApp> {
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
+    if (kDebugMode) {
+      PluginManager.instance
+        ..register(WidgetInfoInspector())
+        ..register(WidgetDetailInspector())
+        ..register(ColorSucker())
+        ..register(AlignRuler())
+        ..register(ColorPicker())
+        ..register(TouchIndicator())
+        ..register(Performance())
+        ..register(ShowCode())
+        ..register(MemoryInfoPage())
+        ..register(CpuInfoPage())
+        ..register(DeviceInfoPanel())
+        ..register(Console())
+        ..register(DioInspector(dio: dio));
+    }
+  }
+
+  Widget _buildApp(BuildContext context) {
     return MaterialApp(
       title: 'UME Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: HomePage(title: 'UME Demo Home Page'),
       onGenerateRoute: (settings) {
         switch (settings.name) {
           case 'detail':
             return MaterialPageRoute(
-                builder: (BuildContext context) => DetailPage());
+              builder: (BuildContext context) => DetailPage(),
+            );
           default:
             return null;
         }
       },
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Widget body = _buildApp(context);
+    if (kDebugMode) {
+      return MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (_) => UMESwitch()),
+        ],
+        builder: (BuildContext context, _) => UMEWidget(
+          enable: context.watch<UMESwitch>().enable,
+          child: body,
+        ),
+      );
+    }
+    return body;
   }
 }
