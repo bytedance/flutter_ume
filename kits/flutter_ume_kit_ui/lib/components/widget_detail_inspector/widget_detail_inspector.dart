c281caf176d5ac6306cf39540f3d6caf3edb02c9
Atsumi3 <atsumi@mail.bizen.jp>
2023-06-06 16:02:11 +0900

    fix conflicted SearchBar

--- kits/flutter_ume_kit_ui/lib/components/widget_detail_inspector/widget_detail_inspector.dart
+++ kits/flutter_ume_kit_ui/lib/components/widget_detail_inspector/widget_detail_inspector.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_ume_kit_ui/components/hit_test.dart';
 import 'package:flutter_ume/flutter_ume.dart';
 import 'package:flutter_ume_kit_ui/util/binding_ambiguate.dart';
-import 'search_bar.dart';
+import 'search_bar.dart' as search_bar;
 import 'icon.dart' as icon;
 
 class WidgetDetailInspector extends StatelessWidget implements Pluggable {
@@ -199,7 +199,7 @@ class __InfoPageState extends State<_InfoPage> {
               Padding(
                 padding: const EdgeInsets.only(
                     left: 12, right: 12, top: 10, bottom: 10),
-                child: SearchBar(
+                child: search_bar.SearchBar(
                     placeHolder: '请输入要搜索的widget', onChangeHandle: _textChange),
               ),
               Expanded(
